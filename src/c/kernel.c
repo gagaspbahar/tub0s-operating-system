4427a5be405b89dd4094100fbc8ce5d2e4a485e4
Gagas Praharsa Bahar <gagaspbahar@G-Laptop.localdomain>
2022-02-23 20:53:02 +0700

    init barebones

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -5,3 +5,33 @@
 // TODO : Tambahkan implementasi kode C
 
 #include "header/kernel.h"
+
+int main()
+{
+    char *string = "Hai"; // Deklarasikan variabel pada awal scope
+    int i = 0;
+    for (i = 0; i < 3; i++)
+    {
+        byte warna = 0xD;
+        putInMemory(0xB000, 0x8000 + 2 * i, string[i]);
+        putInMemory(0xB000, 0x8001 + 2 * i, warna);
+    }
+}
+
+// TODO
+void handleInterrupt21(int AX, int BX, int CX, int DX)
+{
+    // Definisi kosong
+}
+
+void printString(char *string)
+{
+}
+
+void readString(char *string)
+{
+}
+
+void clearScreen()
+{
+}
--- src/c/std_lib.c
+++ src/c/std_lib.c
@@ -1,3 +1,25 @@
 #include "header/std_lib.h"
 
 // Opsional : Tambahkan implementasi kode C
+
+// TODO
+int div(int a, int b) {}
+
+int mod(int a, int n) {}
+
+// Operasi standar bahasa C
+// Implementasikan
+void memcpy(byte *dest, byte *src, unsigned int n) {}
+// Mengcopy n bytes yang ditunjuk src ke dest
+
+unsigned int strlen(char *string) {}
+// Mengembalikan panjang suatu null terminated string
+
+bool strcmp(char *s1, char *s2) {}
+// Mengembalikan true jika string sama
+
+void strcpy(char *dst, char *src) {}
+// Melakukan penyalinan null terminated string
+
+void clear(byte *ptr, unsigned int n) {}
+// Mengosongkan byte array yang memiliki panjang n
