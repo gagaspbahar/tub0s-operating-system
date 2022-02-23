18c37780eccd80b5029fe9b0ba50ef3456950aa0
ubaidalih <ubayariqprathama@gmail.com>
2022-02-23 10:10:24 +0700

    (fix) : fix strcmp typo

--- src/c/std_lib.c
+++ src/c/std_lib.c
@@ -43,7 +43,7 @@ bool strcmp(char *s1, char *s2) {
     while(s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0'){
         i++;
     }
-    return (s1[i] == '\0' && s1[i] == '\0');
+    return (s1[i] == '\0' && s2[i] == '\0');
 }
 
 void strcpy(char *dst, char *src) {
