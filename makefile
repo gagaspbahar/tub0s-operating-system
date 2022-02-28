4c35d5adc3646e26ee32dc188de992b05854852c
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2022-03-01 01:43:24 +0700

    add clearscreen printstring

--- makefile
+++ makefile
@@ -3,23 +3,20 @@ all: diskimage bootloader stdlib kernel
 
 # Recipes
 diskimage:
-	# TODO : Tambahkan untuk pembuatan image
 	dd if=/dev/zero of=out/system.img bs=512 count=2880
 
 bootloader:
-	# TODO : Tambahkan untuk pembuatan bootloader
 	nasm src/asm/bootloader.asm -o out/bootloader
 	dd if=out/bootloader of=out/system.img bs=512 count=1 conv=notrunc
 
 kernel:
-	# TODO : Tambahkan untuk pembuatan kernel
 	bcc -ansi -c -o out/kernel.o src/c/kernel.c
 	nasm -f as86 src/asm/kernel.asm -o out/kernel_asm.o
-	ld86 -o out/kernel -d out/kernel.o out/kernel_asm.o
+	ld86 -o out/kernel -d out/kernel.o out/kernel_asm.o out/std_lib.o
 	dd if=out/kernel of=out/system.img bs=512 conv=notrunc seek=1
 
 stdlib:
-	# Opsional
+	bcc -ansi -c -o out/std_lib.o src/c/std_lib.c
 
 run:
 	bochs -f src/config/if2230.config
--- src/c/kernel.c
+++ src/c/kernel.c
@@ -5,17 +5,18 @@
 // TODO : Tambahkan implementasi kode C
 
 #include "header/kernel.h"
+#include "header/std_lib.h"
 
 int main()
 {
-    char *string = "Hai"; // Deklarasikan variabel pada awal scope
-    int i = 0;
-    for (i = 0; i < 3; i++)
-    {
-        byte warna = 0xD;
-        putInMemory(0xB000, 0x8000 + 2 * i, string[i]);
-        putInMemory(0xB000, 0x8001 + 2 * i, warna);
-    }
+    char buf[128];
+    clearScreen();
+    makeInterrupt21();
+    printString("Halo dunia!\r\n");
+    readString(buf);
+    printString(buf);
+    
+    while (true);
 }
 
 // TODO
@@ -50,8 +51,40 @@ void printString(char *string)
 
 void readString(char *string)
 {
+    int ch;
+    int idx = 0;
+    while (true)
+    {
+        ch = interrupt(0x16, 0, 0, 0, 0);
+        if (ch == '\r')
+        {
+            string[idx] = '\0';
+            interrupt(0x10, 0x0E00 + '\n', 0, 0, 0); // LF
+            interrupt(0x10, 0x0E00 + '\r', 0, 0, 0); // CR
+            break;
+        }
+        else if (ch = '\b')
+        {
+            if (idx > 0 && mod(idx, 80) != 0)
+            {
+                interrupt(0x10, 0x0E00 + '\b', 0, 0, 0); // backspace
+                interrupt(0x10, 0x0E00 + '\0', 0, 0, 0); // terminator
+                interrupt(0x10, 0x0E00 + '\b', 0, 0, 0); // backspace
+                idx--;
+            }
+        }
+        else
+        {
+            string[idx] = ch;
+            interrupt(0x10, 0x0E00 + ch, 0, 0, 0);
+            idx++;
+        }
+    }
 }
 
 void clearScreen()
 {
+    interrupt(0x10, 0x3, 0, 0, 0);
+    interrupt(0x10, 0x200, 0, 0, 0);
+    putInMemory(0xB000, 0x8001, 0xF);
 }
