0bd6440909a447338e8bfc7ae3932834b8447b61
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2022-03-27 19:47:07 +0700

    add kit milestone2

--- src/c/header/kernel.h
+++ src/c/header/kernel.h
@@ -2,6 +2,8 @@
 
 #include "std_type.h"
 #include "std_lib.h"
+#include "std_datatype.h"
+
 
 // Fungsi bawaan
 extern void putInMemory(int segment, int address, byte b);
@@ -10,8 +12,16 @@ extern void makeInterrupt21();
 
 void handleInterrupt21(int AX, int BX, int CX, int DX);
 
+void fillKernelMap();
 
 // Implementasikan
 void printString(char *string);
 void readString(char *string);
 void clearScreen();
+void writeSector(byte *buffer, int sector_number);
+void readSector(byte *buffer, int sector_number);
+
+void write(struct file_metadata *metadata, enum fs_retcode *return_code);
+void read(struct file_metadata *metadata, enum fs_retcode *return_code);
+
+void shell();
