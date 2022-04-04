f89193fa4da9d1f02d929a2612a5729e5e6aa628
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2022-04-05 02:36:57 +0700

    remove shell and splitparam

--- src/c/shell.c
+++ src/c/shell.c
@@ -3,33 +3,6 @@
 #include "header/filesystem.h"
 #include "header/kernel.h"
 
-void printCWD(path_str, current_dir){
-
-}
-
-char* splitParam(){
-  return;
-}
-
-void shell(){
-  char input_buf[64];
-  char path_str[128];
-  byte current_dir = FS_NODE_P_IDX_ROOT;
-
-  while (true) {
-    printString("OS@IF2230:");
-    printCWD(path_str, current_dir);
-    printString("$");
-    readString(input_buf);
-   
-    if (strcmp(input_buf, "cd")){
-      // Utility cd
-    }
-    else 
-      printString("Unknown command\r\n");
-  }
-}
-
 void cd(char *param){
 // cd dapat memindah current working directory ke folder tujuan
 // cd dapat naik satu tingkat dengan argumen “..”
